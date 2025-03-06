5750f3155e27a46c6a457cecb54aa4d576c86f7e
Jeff Wu <jzw121@ic.ac.uk>
2025-03-06 14:46:01 +0000

    installation script

--- 00_installPackages.R
+++ 00_installPackages.R
@@ -4,3 +4,13 @@ install.packages("copula")
 
 library(dataHelper) # check if the library is already installed
 library(dataHelper)
+
+library(did)
+install.packages("lava")
+install.packages("BH")
+install.packages("did")
+
+library(devtools)
+install.packages("devtools")
+
+devtools::install_github("jeffzifanwu/dataHelper")
